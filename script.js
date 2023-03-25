25da93e0331b26858847957646c5c3f398a3e695
aodl <andreina.orte@gmail.com>
2023-03-25 15:17:36 +1000

    basic game logic

---
+++ script.js
@@ -0,0 +1,111 @@
+const words = [
+  "apple",
+  "amber",
+  "angle",
+  "angel",
+  "brave",
+  "bliss",
+  "craft",
+  "crave",
+  "dwarf",
+  "ditch",
+  "eager",
+  "elite",
+  "fable",
+  "feast",
+  "gloom",
+  "giant",
+  "honor",
+  "haste",
+  "ideal",
+  "image",
+  "jolly",
+  "jumpy",
+  "knead",
+  "knock",
+  "lucid",
+  "lucky",
+  "merry",
+  "mirth",
+  "noble",
+  "nerve",
+  "ocean",
+  "olive",
+  "pilot",
+  "peach",
+  "quick",
+  "quiet",
+  "roast",
+  "rally",
+  "silly",
+  "sugar",
+  "tulip",
+  "tiger",
+  "urban",
+  "uncle",
+  "vivid",
+  "venus",
+  "witty",
+  "wager",
+  "xenon",
+  "xylem",
+  "yield",
+  "yacht",
+  "zebra",
+  "zesty",
+];
+let playerString = [];
+let singleString = "";
+let inputFields = document.getElementsByClassName("row-1");
+let winningWord = words[2];
+let wordFound = false;
+let testButton = document.getElementById("test-button");
+// this event will trigger the game to run the checking functions
+testButton.addEventListener("click", function () {
+  for (let input of inputFields) {
+    playerString.push(input.value);
+    singleString = singleString + input.value;
+  }
+  for (let val of words) {
+    if (val === singleString) {
+      console.log("valid word");
+      wordFound = true;
+      if (singleString === winningWord) {
+        console.log("you win");
+        wordFound = false;
+        break;
+      } else {
+        console.log("try again");
+        break;
+      }
+    }
+  }
+
+  if (wordFound) {
+    sharedChars(singleString, winningWord);
+  }
+  console.log(singleString);
+  wordFound = false;
+  singleString = "";
+});
+
+function sharedChars(str1, str2) {
+  for (let i = 0; i < str1.length; i++) {
+    if (str2.indexOf(str1[i]) !== -1) {
+      console.log(str1[i]);
+      if (str2.indexOf(str1[i]) === str1.indexOf(str1[i])) {
+        console.log(
+          `Winning Word index is ${str2.indexOf(
+            str1[i]
+          )} and INPUT index is ${str1.indexOf(str1[i])} - same`
+        );
+      } else {
+        console.log(
+          `Winning Word index is ${str2.indexOf(
+            str1[i]
+          )} and INPUT index is ${str1.indexOf(str1[i])} - different`
+        );
+      }
+    }
+  }
+}
