48ac9ac94a09535cdb3c7f6213f7ecbf9092c9cc
aodl <andreina.orte@gmail.com>
2023-03-31 21:37:08 +1000

    draft README file and styled version of the game

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # wordle-app
+
+# WARDLE - Wordle Clone
+
+This is a simple Wordle clone game built with HTML, CSS, and JavaScript.
+
+## Getting Started
+
+To play the game, simply open the `index.html` file in your browser. You can also play the game online by visiting the [GitHub Pages site](https://yourusername.github.io/wordle-clone/) for this project.
+
+## How to Play
+
+The goal of the game is to guess the secret five-letter word within six attempts. To make a guess, type a five-letter word into the input field and press the "Guess" button. The game will then display a row of colored circles to indicate how many letters are correct and in the right position (red circle) and how many letters are correct but in the wrong position (yellow circle).
+
+## Features
+
+- Randomly generated secret five-letter word
+- Feedback on each guess.
+  - Yellow represents a letter that is present in the word but at a different location.
+  - Green represents a letter that is present in the word and at the same location.
+- Responsive design
+- On-screen keyboard (currently commented out)
+- Timer to make the game more challenging (coming soon)
+- Spanish language support (coming soon)
+
+This is a JavaScript code that creates a word game where the player has to guess a five-letter word from a list of given words. The game is played on a grid with six rows, each consisting of five input fields where the player can input one letter at a time. The player has to guess the letters of the word in the correct order, and for each row, the player is given feedback on how many letters they have guessed correctly.
+
+The game starts by dynamically creating the game board, which consists of six rows of five input fields each. The first row is enabled, and the player can enter a letter in each field. When the player presses Enter, the program checks if the letters in the input fields match any of the words in the given list of words. If the player has guessed the correct letters, the program enables the next row, and the player can continue to the next row. If the player has guessed all the letters correctly, the program declares the player the winner.
+
+The program includes functions for validating the player's input, checking if the input matches any of the words in the list, and enabling/disabling rows on the game board. The program also includes variables for the winning word, the number of elements in the game grid, and several HTML elements that are used to display feedback to the player.
+
+## Technologies Used
+
+- HTML
+- CSS
+- JavaScript
+
+## Acknowledgments
+
+This project was inspired by the popular game Wordle originally created by Josh Wardle .
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details.
--- script.js
+++ script.js
@@ -59,13 +59,39 @@ const words = [
 ];
 let singleString = "";
 const gameContainer = document.getElementById("game-container");
-let inputRowsAll = document.getElementsByClassName("test-input");
-let inputRow1 = document.getElementsByClassName("row-1");
-let inputRow2 = document.getElementsByClassName("row-2");
-let inputRow3 = document.getElementsByClassName("row-3");
-let inputRow4 = document.getElementsByClassName("row-4");
-let inputRow5 = document.getElementsByClassName("row-5");
-let inputRow6 = document.getElementsByClassName("row-6");
+let gameRows = [];
+let numberOfElements = 30; // sets the number of elements I want in my grid
+let warning = document.getElementById("no-word");
+const resetGame = document.getElementById("new-game");
+const giveUpGame = document.getElementById("text-gave-up");
+const textWon = document.getElementById("text-won");
+const giveUpButton = document.getElementById("give-up");
+const alertContainer = document.getElementById("alerts");
+resetGame.addEventListener("click", newGame);
+giveUpButton.addEventListener("click", giveUp);
+
+// dynamic creation of the game board
+for (let i = 0; i < numberOfElements + 1; i++) {
+  let row = document.createElement("input");
+  gameRows.push(row);
+  gameRows[i].setAttribute("id", `${i}`);
+  gameRows[i].maxLength = 1;
+}
+let inputRow1 = createRow(gameRows, 0);
+inputRow1[0].focus(); // focus on first input element on page load
+console.log(inputRow1[0]);
+let inputRow2 = createRow(gameRows, 1);
+disableRow(inputRow2);
+let inputRow3 = createRow(gameRows, 2);
+disableRow(inputRow3);
+let inputRow4 = createRow(gameRows, 3);
+disableRow(inputRow4);
+let inputRow5 = createRow(gameRows, 4);
+disableRow(inputRow5);
+let inputRow6 = createRow(gameRows, 5);
+disableRow(inputRow6);
+let inputRowsAll = document.body.getElementsByClassName("test-input"); //creation of game board ends here
+
 let stringArray = [];
 let winningWord = ["m", "o", "o", "s", "e"];
 let winningWordString = "moose";
@@ -73,6 +99,7 @@ let testFocus = false;
 let wordFound = false;
 // this event will trigger the game to run the checking functions
 let progressVar = 0;
+
 gameContainer.addEventListener("keypress", function (event) {
   if (event.key === "Enter") {
     if (progressVar === 0) {
@@ -93,6 +120,16 @@ gameContainer.addEventListener("keypress", function (event) {
 
 handleInputFocus(inputRowsAll);
 
+function createRow(elements, rowNumber) {
+  let rowForGame = [];
+  rowNumber = rowNumber * 5; /// this number will depend on the number of letters you want in the word
+  for (let i = rowNumber; i < rowNumber + 5; i++) {
+    elements[i].setAttribute("class", `test-input row-${rowNumber / 5}`);
+    gameContainer.appendChild(elements[i]);
+  }
+  rowForGame = document.getElementsByClassName(`row-${rowNumber / 5}`);
+  return rowForGame;
+}
 function validateAnswer(currentRow, nextRow) {
   checkAnswer(currentRow);
   if (testFocus) {
@@ -109,13 +146,13 @@ function checkAnswer(rowArray) {
   for (let val of words) {
     if (val === singleString) {
       testFocus = true;
-      console.log("valid word");
       wordFound = true;
       if (singleString === winningWordString) {
         console.log("you win");
         sharedCharsArray(stringArray, winningWord, rowArray);
         for (let val of rowArray) {
-          val.style["background-color"] = "green";
+          val.style["background-color"] = "#459943";
+          warning.style.display = "none";
         }
         wordFound = false;
         //Disable played row
@@ -126,7 +163,6 @@ function checkAnswer(rowArray) {
         //Disable played row
         disableRow(rowArray);
         progressVar += 1;
-
         break;
       }
     }
@@ -135,7 +171,14 @@ function checkAnswer(rowArray) {
   if (wordFound) {
     sharedCharsArray(stringArray, winningWord, rowArray);
   } else {
-    console.log("this word is not on the word list");
+    alertContainer.style.height = "fit-content";
+    warning.style.transition =
+      "background-color 0.5s linear, height 0.3s linear";
+    warning.style.visibility = "visible";
+    warning.style["background-color"] = "#3c556d";
+    warning.style.height = "24px";
+    warning.style.width = "fit-content";
+    warning.textContent = "WORD NOT IN LIST";
     testFocus = false;
   }
   // resets the value of singleString and wordFound each time event occurs.
@@ -179,9 +222,18 @@ function handleInputFocus(rowArray) {
       });
       input.addEventListener("keydown", function (event) {
         //deletes input value and focuses on previous when backspace is pressed
-        if (event.key === "Backspace") {
+        if (
+          event.key === "Backspace" &&
+          input.previousElementSibling !== null
+        ) {
           input.value = "";
           input.previousElementSibling.focus();
+          warning.style.height = "0px";
+          warning.style.width = "0px";
+          warning.style.visibility = "hidden";
+          warning.style["background-color"] = "#ee8471";
+          warning.style.visibility = "hidden";
+          alertContainer.style.height = "0px";
         }
       });
     }
@@ -199,3 +251,49 @@ function enableRow(rows) {
     input.disabled = false;
   }
 }
+
+function newGame() {
+  let resetOption = window.confirm("Are you sure you want to play a new game?");
+  if (resetOption) {
+    window.location.reload();
+  }
+}
+function giveUp() {
+  let giveUpOption = window.confirm(
+    "Are you sure you want to know the answer?"
+  );
+  if (giveUpOption) {
+    giveUpGame.style.visibility = "visible";
+    giveUpGame.textContent = `ANSWER: ${winningWordString.toUpperCase()}`;
+    giveUpGame.style.width = "fit-content";
+    giveUpGame.style.height = "60px";
+
+    for (let val of gameRows) {
+      val.disabled = true;
+    }
+  }
+}
+// let output = document.getElementsByClassName("test-input");
+// let keys = document.getElementsByClassName("key");
+// for (let val of keys) {
+//   val.textContent = val.textContent;
+// }
+
+// for (let keyElement of keys) {
+//   let key = keyElement.textContent;
+//   keyElement.addEventListener("click", function () {
+//     switch (key) {
+//       case "␡":
+//         output.textContent = output.textContent.slice(
+//           0,
+//           output.textContent.length - 1
+//         );
+//         break;
+//       case "␡ all":
+//         output.textContent = "";
+//         break;
+//       default:
+//         document.hasFocus.value = "a";
+//     }
+//   });
+// }
